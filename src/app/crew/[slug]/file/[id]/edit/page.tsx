4ca79865fc429d3e5d8c4efcb2dea962f79c4e00
qarq90 <scythethroughtheheart05@gmail.com>
2025-08-03 13:35:51 +0530

    Build - 03/08/2025

--- src/app/crew/[slug]/file/[id]/edit/page.tsx
+++ src/app/crew/[slug]/file/[id]/edit/page.tsx
@@ -1,9 +1,18 @@
-import { use } from "react";
 import { BodyWrapper } from "@/components/wrappers";
 import Client from "./client";
 
-export default function CreateFile({ params }: { params: Promise<{ slug: string; id: string }> }) {
-    const { slug, id } = use(params);
+type PageProps = {
+    params: {
+        slug: string;
+        id: string;
+    };
+};
+
+export default async function CreateFile({ params }: PageProps) {
+    const { slug, id } = params;
+
+    // You can also do async data fetching here if needed
+    // const data = await fetch(...)
 
     return (
         <BodyWrapper>
